ca8e17243a5de8118c395708949afdb1a7d0ad66
brayanbertan <brayanbertan@gmail.com>
2021-04-08 10:44:33 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/tutorialedge/go-fiber-tutorial
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	gorm.io/gorm v1.21.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/tutorialedge/go-fiber-tutorial/book"
+	"github.com/tutorialedge/go-fiber-tutorial/database"
+)
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, World!")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", helloWorld)
+
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("postgres", "postgres://xqdklxpmcynwxv:110df239439cd5370cc7a27338b790efd440f161e099bce7997b57a2cc4cc879@ec2-18-214-208-89.compute-1.amazonaws.com:5432/d800se307n7p4s")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
